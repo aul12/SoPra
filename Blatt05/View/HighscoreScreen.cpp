2cce2bec72d891a0d42969476993dedb47bdef2e
Paul Nykiel <paul.nykiel@gmail.com>
2018-12-24 16:00:48 +0100

    Maybe fixed CI

---
+++ Blatt05/View/HighscoreScreen.cpp
@@ -0,0 +1,8 @@
+/**
+ * @file HighscoreScreen.cpp
+ * @author paul
+ * @date 24.12.18
+ * @brief HighscoreScreen @TODO
+ */
+
+#include "HighscoreScreen.hpp"
---
+++ Blatt05/View/HighscoreScreen.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file HighscoreScreen.hpp
+ * @author paul
+ * @date 24.12.18
+ * @brief HighscoreScreen @TODO
+ */
+
+#ifndef TEST_HIGHSCORESCREEN_HPP
+#define TEST_HIGHSCORESCREEN_HPP
+
+
+class HighscoreScreen {
+
+};
+
+
+#endif //TEST_HIGHSCORESCREEN_HPP
